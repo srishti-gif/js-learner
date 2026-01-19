19c470d1729c1d0a3f6e161d1a80c4b703d2bb21
Cosmos <srishti@uixlabs.in>
2026-01-20 02:29:31 +0530

    Completed Day-06 Arrays Assignment

--- Day-06/Arrays-Assignment/app.js
+++ Day-06/Arrays-Assignment/app.js
@@ -1,15 +1,6 @@
 /*
 T-001: Create an array of 5 elements using the Array Constructor.
- T-002: Create an array of 3 empty slots.
- T-003: Create an array of 6 elements using the Array literals and access the fourth element in the array using its length property.
- T-004: Use the for loop on the above array to print elements in the odd index.
- T-005: Add one element at the front and the end of an array.
- T-006: Remove an element from the front and the end of an array.
- T-007: Create an array containing the name of your favourite foods(10 foods). Destructure the 6th food element from the array using destructuring.
-
 */
-
-//creates arrsy with 5 elements.
 const newArr = new Array(1, 2, 3, 4, 5);
 console.log(newArr);
 
@@ -71,14 +62,7 @@ console.log(sixthElement);
 
 /*
  T-008: Take out the last 8 food items from the above array using the Array destructuring. Hint: rest parameter.
- T-009: Clone an Array(Shallow cloning)
- T-010: Empty an array using its length property
- T-011: Create an array of 10 elements(number 1 to 10). Resize the array to length 6 once you find the number 5 in that array. Hint: Use for-loop.
- T-012: Create an Array of 10 elements. Use the splice() method to empty the array.
- T-013: Create an Array of 10 elements. 
- You can empty the array in multiple ways: using the length property, using the pop() method, using the shift() method, setting the array with [], or the splice() method. Which among these methods are most efficient and why?
 */
-
 //Take out the last 8 food items from the above array using the Array destructuring.
 // Hint: rest parameter.
 
@@ -90,6 +74,7 @@ const copiedArr = fruits;
 console.log(copiedArr);
 console.log(fruits);
 
+// T-009: Clone an Array(Shallow cloning)
 copiedArr.pop(); // shallow copy
 console.log(copiedArr);
 console.log(fruits);
@@ -131,15 +116,7 @@ console.log(nums);
 
 /*
  T-014: What happens when you concatenate two empty arrays?
- T-015: How can you check if a value is partially matching with any of the elements of an Array?
- T-016: What is the difference between the slice() and splice() methods?
- T-017: Create an Array of alphanumeric strings. Sort the elements in both ascending and descending orders. You must be doing this in an immutable way such that the source array never gets modified.
- T-018: Can you give examples of sparse and dense arrays?
- T-019: Give a practical usages of the .fill() method
- T-020: How to convert an array to a string?
-
 */
-
 nums = [1, 2, 3, 4];
 nums2 = [6, 7, 8, 9];
 
